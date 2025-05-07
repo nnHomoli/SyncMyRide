f1ea8b351d742e6e711e109c25c2263ea2c25eee
nnHomoli <mrfreedoma271@gmail.com>
2025-05-08 03:38:45 +0700

    allow versions beyond 7.3_01 make mobs drop passengers on death

---
+++ src/main/java/nnhomoli/syncmyride/mixins/mobMixin.java
@@ -0,0 +1,18 @@
+package nnhomoli.syncmyride.mixins;
+
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.Mob;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = Mob.class,remap=false)
+public class mobMixin {
+	@Inject(method="onDeath",at=@At("HEAD"))
+	public void onDeath(Entity entityKilledBy, CallbackInfo ci) {
+		Mob m = (Mob)(Object) this;
+		m.ejectRider();
+	}
+}
